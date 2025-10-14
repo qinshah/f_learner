9be57cc6d000ac7323cc206d8d4b2daa3d950482
qshh <3218696007@qq.com>
2025-10-15 04:28:15 +0800

    鸿蒙报错

--- lib/module.old/rich_text/fleather_page.dart
+++ lib/module.old/rich_text/fleather_page.dart
@@ -1,219 +1,220 @@
-import 'dart:convert';
-import 'dart:io';
+// 鸿蒙报错
+// import 'dart:convert';
+// import 'dart:io';
 
-import 'package:fleather/fleather.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:fleather/fleather.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-class FleatherPage extends StatefulWidget {
-  const FleatherPage({super.key});
+// class FleatherPage extends StatefulWidget {
+//   const FleatherPage({super.key});
 
-  @override
-  State<FleatherPage> createState() => _FleatherPageState();
-}
+//   @override
+//   State<FleatherPage> createState() => _FleatherPageState();
+// }
 
-class _FleatherPageState extends State<FleatherPage> {
-  final FocusNode _focusNode = FocusNode();
-  final GlobalKey<EditorState> _editorKey = GlobalKey();
-  FleatherController? _controller;
+// class _FleatherPageState extends State<FleatherPage> {
+//   final FocusNode _focusNode = FocusNode();
+//   final GlobalKey<EditorState> _editorKey = GlobalKey();
+//   FleatherController? _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    if (kIsWeb) BrowserContextMenu.disableContextMenu();
-    _initController();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (kIsWeb) BrowserContextMenu.disableContextMenu();
+//     _initController();
+//   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    if (kIsWeb) BrowserContextMenu.enableContextMenu();
-  }
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     if (kIsWeb) BrowserContextMenu.enableContextMenu();
+//   }
 
-  Future<void> _initController() async {
-    try {
-      final result = await rootBundle.loadString('assets/welcome.json');
-      final heuristics = ParchmentHeuristics(
-        formatRules: [],
-        insertRules: [
-          ForceNewlineForInsertsAroundInlineImageRule(),
-        ],
-        deleteRules: [],
-      ).merge(ParchmentHeuristics.fallback);
-      final doc = ParchmentDocument.fromJson(
-        jsonDecode(result),
-        heuristics: heuristics,
-      );
-      _controller = FleatherController(document: doc);
-    } catch (err, st) {
-      if (kDebugMode) {
-        print('Cannot read welcome.json: $err\n$st');
-      }
-      _controller = FleatherController();
-    }
-    setState(() {});
-  }
+//   Future<void> _initController() async {
+//     try {
+//       final result = await rootBundle.loadString('assets/welcome.json');
+//       final heuristics = ParchmentHeuristics(
+//         formatRules: [],
+//         insertRules: [
+//           ForceNewlineForInsertsAroundInlineImageRule(),
+//         ],
+//         deleteRules: [],
+//       ).merge(ParchmentHeuristics.fallback);
+//       final doc = ParchmentDocument.fromJson(
+//         jsonDecode(result),
+//         heuristics: heuristics,
+//       );
+//       _controller = FleatherController(document: doc);
+//     } catch (err, st) {
+//       if (kDebugMode) {
+//         print('Cannot read welcome.json: $err\n$st');
+//       }
+//       _controller = FleatherController();
+//     }
+//     setState(() {});
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(elevation: 0, title: const Text('Fleather富文本')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final picker = ImagePicker();
-          final image = await picker.pickImage(source: ImageSource.gallery);
-          if (image != null) {
-            final selection = _controller!.selection;
-            _controller!.replaceText(
-              selection.baseOffset,
-              selection.extentOffset - selection.baseOffset,
-              EmbeddableObject('image', inline: false, data: {
-                'source_type': kIsWeb ? 'url' : 'file',
-                'source': image.path,
-              }),
-            );
-            _controller!.replaceText(
-              selection.baseOffset + 1,
-              0,
-              '\n',
-              selection:
-                  TextSelection.collapsed(offset: selection.baseOffset + 2),
-            );
-          }
-        },
-        child: const Icon(Icons.add_a_photo),
-      ),
-      body: _controller == null
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                FleatherToolbar.basic(
-                    controller: _controller!, editorKey: _editorKey),
-                Divider(height: 1, thickness: 1, color: Colors.grey.shade200),
-                Expanded(
-                  child: FleatherEditor(
-                    controller: _controller!,
-                    focusNode: _focusNode,
-                    editorKey: _editorKey,
-                    padding: EdgeInsets.only(
-                      left: 16,
-                      right: 16,
-                      bottom: MediaQuery.of(context).padding.bottom,
-                    ),
-                    onLaunchUrl: _launchUrl,
-                    maxContentWidth: 800,
-                    embedBuilder: _embedBuilder,
-                    spellCheckConfiguration: SpellCheckConfiguration(
-                        spellCheckService: DefaultSpellCheckService(),
-                        misspelledSelectionColor: Colors.red,
-                        misspelledTextStyle:
-                            DefaultTextStyle.of(context).style),
-                  ),
-                ),
-              ],
-            ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(elevation: 0, title: const Text('Fleather富文本')),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () async {
+//           final picker = ImagePicker();
+//           final image = await picker.pickImage(source: ImageSource.gallery);
+//           if (image != null) {
+//             final selection = _controller!.selection;
+//             _controller!.replaceText(
+//               selection.baseOffset,
+//               selection.extentOffset - selection.baseOffset,
+//               EmbeddableObject('image', inline: false, data: {
+//                 'source_type': kIsWeb ? 'url' : 'file',
+//                 'source': image.path,
+//               }),
+//             );
+//             _controller!.replaceText(
+//               selection.baseOffset + 1,
+//               0,
+//               '\n',
+//               selection:
+//                   TextSelection.collapsed(offset: selection.baseOffset + 2),
+//             );
+//           }
+//         },
+//         child: const Icon(Icons.add_a_photo),
+//       ),
+//       body: _controller == null
+//           ? const Center(child: CircularProgressIndicator())
+//           : Column(
+//               children: [
+//                 FleatherToolbar.basic(
+//                     controller: _controller!, editorKey: _editorKey),
+//                 Divider(height: 1, thickness: 1, color: Colors.grey.shade200),
+//                 Expanded(
+//                   child: FleatherEditor(
+//                     controller: _controller!,
+//                     focusNode: _focusNode,
+//                     editorKey: _editorKey,
+//                     padding: EdgeInsets.only(
+//                       left: 16,
+//                       right: 16,
+//                       bottom: MediaQuery.of(context).padding.bottom,
+//                     ),
+//                     onLaunchUrl: _launchUrl,
+//                     maxContentWidth: 800,
+//                     embedBuilder: _embedBuilder,
+//                     spellCheckConfiguration: SpellCheckConfiguration(
+//                         spellCheckService: DefaultSpellCheckService(),
+//                         misspelledSelectionColor: Colors.red,
+//                         misspelledTextStyle:
+//                             DefaultTextStyle.of(context).style),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//     );
+//   }
 
-  Widget _embedBuilder(BuildContext context, EmbedNode node) {
-    if (node.value.type == 'icon') {
-      final data = node.value.data;
-      // Icons.rocket_launch_outlined
-      return Icon(
-        IconData(int.parse(data['codePoint']), fontFamily: data['fontFamily']),
-        color: Color(int.parse(data['color'])),
-        size: 18,
-      );
-    }
+//   Widget _embedBuilder(BuildContext context, EmbedNode node) {
+//     if (node.value.type == 'icon') {
+//       final data = node.value.data;
+//       // Icons.rocket_launch_outlined
+//       return Icon(
+//         IconData(int.parse(data['codePoint']), fontFamily: data['fontFamily']),
+//         color: Color(int.parse(data['color'])),
+//         size: 18,
+//       );
+//     }
 
-    if (node.value.type == 'image') {
-      final sourceType = node.value.data['source_type'];
-      ImageProvider? image;
-      if (sourceType == 'assets') {
-        image = AssetImage(node.value.data['source']);
-      } else if (sourceType == 'file') {
-        image = FileImage(File(node.value.data['source']));
-      } else if (sourceType == 'url') {
-        image = NetworkImage(node.value.data['source']);
-      } else if (sourceType == 'data') {
-        // source: 'data:image/jpeg;base64, LzlqLzRBQ... <!-- Base64 data -->'
-        RegExp regex = RegExp(
-          r'^data:image\/(png|jpe?g|gif|bmp|webp);base64,',
-          caseSensitive: false,
-        );
-        if (regex.hasMatch(node.value.data['source'])) {
-          String base64Image =
-              node.value.data['source'].replaceFirst(regex, '');
-          image = MemoryImage(base64Decode(base64Image));
-        }
-      }
-      if (image != null) {
-        return Padding(
-          // Caret takes 2 pixels, hence not symmetric padding values.
-          padding: const EdgeInsets.only(left: 4, right: 2, top: 2, bottom: 2),
-          child: Container(
-            width: (node.value.data['width'] as num?)?.toDouble() ?? 300,
-            height: (node.value.data['height'] as num?)?.toDouble() ?? 300,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: image, fit: BoxFit.cover),
-            ),
-          ),
-        );
-      }
-    }
+//     if (node.value.type == 'image') {
+//       final sourceType = node.value.data['source_type'];
+//       ImageProvider? image;
+//       if (sourceType == 'assets') {
+//         image = AssetImage(node.value.data['source']);
+//       } else if (sourceType == 'file') {
+//         image = FileImage(File(node.value.data['source']));
+//       } else if (sourceType == 'url') {
+//         image = NetworkImage(node.value.data['source']);
+//       } else if (sourceType == 'data') {
+//         // source: 'data:image/jpeg;base64, LzlqLzRBQ... <!-- Base64 data -->'
+//         RegExp regex = RegExp(
+//           r'^data:image\/(png|jpe?g|gif|bmp|webp);base64,',
+//           caseSensitive: false,
+//         );
+//         if (regex.hasMatch(node.value.data['source'])) {
+//           String base64Image =
+//               node.value.data['source'].replaceFirst(regex, '');
+//           image = MemoryImage(base64Decode(base64Image));
+//         }
+//       }
+//       if (image != null) {
+//         return Padding(
+//           // Caret takes 2 pixels, hence not symmetric padding values.
+//           padding: const EdgeInsets.only(left: 4, right: 2, top: 2, bottom: 2),
+//           child: Container(
+//             width: (node.value.data['width'] as num?)?.toDouble() ?? 300,
+//             height: (node.value.data['height'] as num?)?.toDouble() ?? 300,
+//             decoration: BoxDecoration(
+//               image: DecorationImage(image: image, fit: BoxFit.cover),
+//             ),
+//           ),
+//         );
+//       }
+//     }
 
-    return defaultFleatherEmbedBuilder(context, node);
-  }
+//     return defaultFleatherEmbedBuilder(context, node);
+//   }
 
-  void _launchUrl(String? url) async {
-    if (url == null) return;
-    final uri = Uri.parse(url);
-    final canLaunch = await canLaunchUrl(uri);
-    if (canLaunch) {
-      await launchUrl(uri);
-    }
-  }
-}
+//   void _launchUrl(String? url) async {
+//     if (url == null) return;
+//     final uri = Uri.parse(url);
+//     final canLaunch = await canLaunchUrl(uri);
+//     if (canLaunch) {
+//       await launchUrl(uri);
+//     }
+//   }
+// }
 
-/// This is an example insert rule that will insert a new line before and
-/// after inline image embed.
-class ForceNewlineForInsertsAroundInlineImageRule extends InsertRule {
-  @override
-  Delta? apply(Delta document, int index, Object data) {
-    if (data is! String) return null;
+// /// This is an example insert rule that will insert a new line before and
+// /// after inline image embed.
+// class ForceNewlineForInsertsAroundInlineImageRule extends InsertRule {
+//   @override
+//   Delta? apply(Delta document, int index, Object data) {
+//     if (data is! String) return null;
 
-    final iter = DeltaIterator(document);
-    final previous = iter.skip(index);
-    final target = iter.next();
-    final cursorBeforeInlineEmbed = _isInlineImage(target.data);
-    final cursorAfterInlineEmbed =
-        previous != null && _isInlineImage(previous.data);
+//     final iter = DeltaIterator(document);
+//     final previous = iter.skip(index);
+//     final target = iter.next();
+//     final cursorBeforeInlineEmbed = _isInlineImage(target.data);
+//     final cursorAfterInlineEmbed =
+//         previous != null && _isInlineImage(previous.data);
 
-    if (cursorBeforeInlineEmbed || cursorAfterInlineEmbed) {
-      final delta = Delta()..retain(index);
-      if (cursorAfterInlineEmbed && !data.startsWith('\n')) {
-        delta.insert('\n');
-      }
-      delta.insert(data);
-      if (cursorBeforeInlineEmbed && !data.endsWith('\n')) {
-        delta.insert('\n');
-      }
-      return delta;
-    }
-    return null;
-  }
+//     if (cursorBeforeInlineEmbed || cursorAfterInlineEmbed) {
+//       final delta = Delta()..retain(index);
+//       if (cursorAfterInlineEmbed && !data.startsWith('\n')) {
+//         delta.insert('\n');
+//       }
+//       delta.insert(data);
+//       if (cursorBeforeInlineEmbed && !data.endsWith('\n')) {
+//         delta.insert('\n');
+//       }
+//       return delta;
+//     }
+//     return null;
+//   }
 
-  bool _isInlineImage(Object data) {
-    if (data is EmbeddableObject) {
-      return data.type == 'image' && data.inline;
-    }
-    if (data is Map) {
-      return data[EmbeddableObject.kTypeKey] == 'image' &&
-          data[EmbeddableObject.kInlineKey];
-    }
-    return false;
-  }
-}
+//   bool _isInlineImage(Object data) {
+//     if (data is EmbeddableObject) {
+//       return data.type == 'image' && data.inline;
+//     }
+//     if (data is Map) {
+//       return data[EmbeddableObject.kTypeKey] == 'image' &&
+//           data[EmbeddableObject.kInlineKey];
+//     }
+//     return false;
+//   }
+// }
