e79611fb880ee9c87f4693afaa2f0981b6202207
qshh <3218696007@qq.com>
2025-10-15 15:20:33 +0800

    feat(root): 重构页面视图结构并模块化分类
    - 将系统相关页面从others_page_view移至api_page_view，优化页面结构
    - 在demos_page_view新增导航或路由分类，添加简单底部导航和嵌套路由导航页面
    - 在demos_page_view新增“我的状态管理尝试”分类，调整状态管理页面布局
    - others_page_view去除多余分类，添加TODO提示部分内容移至第三方库
    - 在packages_page_view新增布局组件、游戏开发、动画组件和系统功能分类，丰富组件集合
    - 各页面列表改用CategoryWidget和PageModel3进行统一管理与展示

--- lib/root/page/demos_page_view.dart
+++ lib/root/page/demos_page_view.dart
@@ -2,6 +2,8 @@ import 'package:f_learner/data_model/page_model.dart';
 import 'package:f_learner/module.demo/counter/provider_page_view.dart';
 import 'package:f_learner/module.demo/state_management/vsvl/vsvl_page.dart';
 import 'package:f_learner/module.old/bbs/bbs_page.dart';
+import 'package:f_learner/module.old/nav_or_routes/bottom_nav_page.dart';
+import 'package:f_learner/module.old/nav_or_routes/routes_nav_page.dart';
 import 'package:f_learner/root/category_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +18,11 @@ class DemosPageView extends StatelessWidget {
         CategoryWidget(name: '计数器状态管理', pages: [
           PageModel3('provider', const ProviderPageView()),
         ]),
-        CategoryWidget(name: '状态管理', pages: [
+        CategoryWidget(name: '导航或路由', pages: [
+          PageModel3('简单底部导航', const BottomNavPage()),
+          PageModel3('嵌套路由导航', const RoutesNavPage()),
+        ]),
+        CategoryWidget(name: '我的状态管理尝试', pages: [
           PageModel3('vsvl状态管理', const VsvlPage()),
           PageModel3('通过状态构建', const BbsPage()),
         ]),
