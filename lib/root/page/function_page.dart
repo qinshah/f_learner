779796fcb0707eeaf73cd98e58bb59c0493a9e66
qshh <3218696007@qq.com>
2025-11-15 05:30:02 +0800

    feat(function): 添加文件选择与系统打开功能
    - 新增文件选择页面，支持单选和多选文件/文件夹
    - 实现使用系统应用打开文件的功能
    - 在功能页面中集成文件管理和本地存储分类
    - 更新 file_selector 依赖版本至 1.0.3
    - 修改根视图导航结构，将功能模块移至首页展示

---
+++ lib/root/page/function_page.dart
@@ -0,0 +1,24 @@
+import 'package:f_learner/data_model/page_model.dart';
+import 'package:f_learner/module.function/select_file.dart';
+import 'package:f_learner/module.function/open_file_by_system.dart';
+import 'package:flutter/material.dart';
+
+import '../category_widget.dart';
+
+class FunctionPage extends StatelessWidget {
+  const FunctionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: [
+        CategoryWidget(name: '文件管理', pages: [
+          PageModel3('选取文件/文件夹', SelectFile()),
+          PageModel3('使用系统打开文件', OpenFileBySystem()),
+        ]),
+        CategoryWidget(name: '本地存储', pages: []),
+      ],
+    );
+  }
+}
