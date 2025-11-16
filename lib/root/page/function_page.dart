7e64b544ea67cc6b66865ba9950a5cb1b4c35250
qshh <3218696007@qq.com>
2025-11-16 16:26:47 +0800

    feat(file_management): 增加在文件管理器中显示文件路径功能
    - 新增RevealInFileManager组件，支持选择文件或文件夹并在文件管理器中显示其路径
    - 兼容Windows、macOS和鸿蒙系统，鸿蒙平台使用open_file_ohos打开目录
    - 添加操作结果反馈，支持失败异常捕获提示
    - 在功能页添加“在文件管理器中显示”入口
    - 更新macOS插件注册，新增open_file_macos插件支持
    - 增加相关依赖包os_type和show_in_file_manager，完善跨平台支持

--- lib/root/page/function_page.dart
+++ lib/root/page/function_page.dart
@@ -1,6 +1,7 @@
 import 'package:f_learner/data_model/page_model.dart';
 import 'package:f_learner/module.function/select_file.dart';
 import 'package:f_learner/module.function/open_file_by_system.dart';
+import 'package:f_learner/module.function/reveal_in_file_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../category_widget.dart';
@@ -13,8 +14,12 @@ class FunctionPage extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(8),
       children: [
+        CategoryWidget(name: '设备', pages: [
+          // PageModel3('操作系统类型', GetOSType()),
+        ]),
         CategoryWidget(name: '文件管理', pages: [
           PageModel3('选取文件/文件夹', SelectFile()),
+          PageModel3('在文件管理器中显示', RevealInFileManager()),
           PageModel3('使用系统打开文件', OpenFileBySystem()),
         ]),
         CategoryWidget(name: '本地存储', pages: []),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import open_file_mac
+import open_file_macos
 import path_provider_foundation
 import printing
 import quill_native_bridge_macos
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  OpenFileMacosPlugin.register(with: registry.registrar(forPlugin: "OpenFileMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
